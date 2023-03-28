03e528a090bf1df09b5d6bc988c6830caa73ece4
Texas_Oct <aecom233@gmail.com>
2023-03-28 15:42:02 +0800

    v0.3.3

---
+++ src/main.rs
@@ -0,0 +1,143 @@
+mod operation;
+#[macro_use]
+extern crate rocket;
+use std::{net::IpAddr, str::FromStr};
+use operation::{init_ap, DEVICE, raw2str};
+use rocket::serde::{json::Json, Serialize, Deserialize};
+use serde_json;
+
+#[get("/")]
+fn index() -> &'static str {
+    ""
+}
+
+#[get("/ping")]
+fn ping() -> &'static str {
+    "pong!"
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Config {
+    peer_ip: String,
+    server_socket: String,
+    server_pubkey: String,
+    peer_private_key: String,
+    peer_pubkey: String,
+    peer_ssid: String,
+    peer_passwd: String,
+}
+
+#[get("/get/config")]
+fn get_config() -> Json<Config> {
+    let answer = match std::fs::File::open("config.json") {
+        Ok(mut file) => {
+            let mut stdout = std::io::stdout();
+            let str = &std::io::copy(&mut file, &mut stdout).unwrap().to_string();
+            let data: serde_json::Value = serde_json::from_str(str).unwrap();
+            data
+        }
+        Err(e) => {
+            println!("with error: {}, use existing settings", e);
+            let res;
+            unsafe { res = DEVICE.get_existing_value() }
+            res
+        }
+    };
+
+    Json(Config {
+        peer_ip: answer["peer_ip"].to_string(),
+        server_socket: answer["server_socket"].to_string(),
+        server_pubkey: answer["server_pubkey"].to_string(),
+        peer_private_key: answer["server_private_key"].to_string(),
+        peer_pubkey: answer["server_pubkey"].to_string(),
+        peer_ssid: answer["peer_ssid"].to_string(),
+        peer_passwd: answer["peer_passwd"].to_string(),
+    })
+}
+
+#[post("/update/config", format = "json", data = "<config>")]
+fn update_config(config: Json<Config>) -> &'static str {
+    unsafe {
+        DEVICE.update_peer_ip(
+            IpAddr::from_str(&*config.peer_ip).unwrap());
+        DEVICE.update_server_socket(
+            config.server_socket.clone());
+        DEVICE.update_server_pubkey(
+            config.peer_pubkey.clone());
+        DEVICE.update_peer_ssid(
+            config.peer_ssid.clone());
+        DEVICE.update_peer_passwd(
+            config.peer_passwd.clone());
+        DEVICE.update_new_keypair(
+            config.peer_private_key.clone(),
+            config.peer_pubkey.clone())
+    }
+    "200"
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct KeyPairConfig {
+    pubkey: String,
+    private_key: String
+}
+
+#[post("/update/config")]
+fn gen_new_keypair() -> Json<KeyPairConfig> {
+    let keypair;
+    unsafe{
+        keypair = DEVICE.generate_new_key();
+    }
+    Json(KeyPairConfig{
+        pubkey: raw2str(keypair.public.as_bytes()),
+        private_key: raw2str(keypair.private.as_bytes()),
+    })
+}
+
+#[post("/update/reload")]
+fn reload_config() -> &'static str {
+    unsafe {
+        DEVICE.start_device()
+    }
+    match init_ap("test", "test1") {
+        Ok(_) => "success!",
+        Err(_) => "failed"
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    println!("Start to read config file to start the service");
+    match std::fs::File::open("config.json") {
+        Ok(mut file) => {
+            let mut stdout = std::io::stdout();
+            let str = &std::io::copy(&mut file, &mut stdout).unwrap().to_string();
+            let data: serde_json::Value = serde_json::from_str(str).unwrap();
+            unsafe {
+                DEVICE.update_peer_ip(
+                    IpAddr::from_str(&data["peer_ip"].to_string()).unwrap(),);
+                DEVICE.update_server_socket(
+                    data["server_socket"].to_string());
+                DEVICE.update_server_pubkey(
+                    data["Key"].to_string());
+                DEVICE.start_device();
+            }
+        }
+        Err(e) => {
+            println!("with error: {}, use default settings", e);
+            unsafe {
+                DEVICE.start_device()
+            }
+
+        }
+    }
+
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/ping", routes![ping])
+        .mount("/update/config", routes![update_config])
+        .mount("/update/reload", routes![reload_config])
+        .mount("/get/config", routes![get_config])
+        .mount("/update/keypair",routes![gen_new_keypair])
+}
---
+++ src/operation.rs
@@ -0,0 +1,193 @@
+use rust_uci::Uci;
+use std::error::Error;
+use std::net::{IpAddr, SocketAddr};
+use std::process::exit;
+use std::process::Command;
+use std::str::FromStr;
+use std::string::ToString;
+use wireguard_control::backends::kernel::delete_interface;
+use wireguard_control::{Backend, DeviceUpdate, Key, KeyPair};
+use once_cell::sync::Lazy;
+use serde_json::{json, Value};
+
+pub(crate) static mut DEVICE: Lazy<Peer> = Lazy::new(||{
+    Peer::init(
+        IpAddr::from_str("10.10.1.2").unwrap(),
+        "10.1.1.1:8889".to_string(),
+        "11111111111111111111111111111111".to_string()
+    )
+});
+
+pub struct Peer {
+    peer_ip: IpAddr,
+    server_socket: SocketAddr,
+    server_pubkey: Key,
+    peer_keypair: KeyPair,
+    peer_ssid: String,
+    peer_passwd: String,
+}
+
+fn boot_wireguard_device(
+    peer_ip: &IpAddr,
+    peer_keypair: &KeyPair,
+    server_socket: &SocketAddr,
+    server_pubkey: &Key,
+) -> std::io::Result<()> {
+    DeviceUpdate::new()
+        .set_keypair(peer_keypair.clone())
+        .replace_peers()
+        .add_peer_with(server_pubkey, |peer| {
+            peer.set_endpoint(server_socket.clone())
+                .replace_allowed_ips()
+                .add_allowed_ip(peer_ip.clone(), 0)
+        })
+        .apply(&"mosquitto-wg".parse().unwrap(), Backend::Kernel)
+}
+
+pub fn init_ap(ssid: &str, key: &str) -> Result<(), Box<dyn Error>> {
+    let mut uci = Uci::new()?;
+    uci.set("firewall.@zone[1].network", "wwan")?;
+    uci.commit("firewall")?;
+    Command::new("/etc/init.d/firewall").arg("restart");
+
+    uci.set("network.lan.ipaddr", "192.168.2.1")?;
+    uci.set("network.wwan", "interface")?;
+    uci.set("network.wwan.proto", "dhcp")?;
+    uci.commit("network")?;
+    Command::new("/etc/init.d/network").arg("restart");
+
+    uci.set("wireless.wwan", "wifi-iface")?;
+    uci.set("wireless.wwan.device", "radio0")?;
+    uci.set("wireless.wwan.network", "wlan")?;
+    uci.set("wireless.wwan.mode", "sta")?;
+    uci.set("wireless.wwan.encryption", "none")?;
+    uci.set("wireless.wwan.key", key)?;
+    uci.set("wireless.wwan.ssid", ssid)?;
+    uci.commit("wireless")?;
+    Command::new("wifi").arg("reload");
+
+    Ok(())
+}
+
+fn str2raw(key: String) -> [u8; 32] {
+    let mut raw = [0; 32];
+    if key.len() > 32 || key.len() == 0 {
+        exit(-1)
+    }
+    for (tag, c) in key.chars().enumerate() {
+            raw[tag] = c as u8
+    }
+    raw
+}
+
+pub(crate) fn raw2str(key: &[u8]) -> String {
+    let mut str = String::from("");
+    if key.len() > 32 || key.len() == 0 {
+        exit(-1)
+    }
+    for num in key.iter() {
+        str.push(*num as char)
+    }
+    str
+}
+
+impl Peer {
+    pub fn init(peer_ip: IpAddr, server_socket: String, server_pubkey: String) -> Self {
+        let peer_keypair = KeyPair::generate();
+        println!("The device keypair: {:?}", peer_keypair);
+        Peer {
+            peer_ip,
+            server_socket: server_socket
+                .parse()
+                .expect("Wrong socket address, please check again!"),
+            server_pubkey: Key::from_raw(str2raw(server_pubkey)),
+            peer_keypair,
+            peer_ssid: String::from("mosquitto-ap"),
+            peer_passwd: String::from("12345678"),
+        }
+    }
+
+    pub fn update_server_pubkey(&mut self, key: String) {
+        self.server_pubkey = Key::from_raw(str2raw(key))
+    }
+
+    pub fn update_server_socket(&mut self, socket: String) {
+        self.server_socket = socket.parse().expect("failed to update socket!")
+    }
+
+    pub fn update_peer_ip(&mut self, ip: IpAddr) {
+        self.peer_ip = ip;
+    }
+
+    pub fn update_peer_passwd(&mut self, passwd: String) {
+        self.peer_passwd = passwd;
+    }
+
+    pub fn update_peer_ssid(&mut self, ssid: String) {
+        self.peer_ssid = ssid;
+    }
+
+    pub fn generate_new_key(&mut self) -> KeyPair {
+        let new_keypair = KeyPair::generate();
+        println!("New keypair: {:?}", new_keypair);
+        self.peer_keypair = new_keypair.clone();
+        new_keypair
+    }
+
+    pub fn update_new_keypair(&mut self, private_key: String, _pubkey: String) {
+        self.peer_keypair = KeyPair::from_private(
+            Key::from_raw(str2raw(private_key)));
+    }
+
+    pub fn start_device(&self) {
+        delete_interface(&"mosquitto-wg".parse().unwrap()).expect("No wireguard Interface!");
+        boot_wireguard_device(
+            &self.peer_ip,
+            &self.peer_keypair,
+            &self.server_socket,
+            &self.server_pubkey,
+        )
+            .expect("Failed to build device!");
+        init_ap(&self.peer_ssid, &self.peer_passwd).expect("Failed to start ap!");
+        println!("update wireguard device success!")
+    }
+
+/*    pub fn restart_device(&self) {
+        delete_interface(&"mosquitto-wg".parse().unwrap()).expect("No wireguard Interface!");
+        boot_device(
+            &self.peer_ip,
+            &self.peer_keypair,
+            &self.server_socket,
+            &self.server_pubkey,
+        )
+            .expect("Failed to renew device!");
+        println!("restart device success!")
+    }*/
+
+    /// Rewrite the config file
+    pub fn overwrite_config(&self) {
+        /*TODO*/
+    }
+
+    /// read value from ram
+    pub fn get_existing_value(&self) -> Value {
+        let peer_ip = self.peer_ip.to_string();
+        let server_socket = self.server_socket.to_string();
+        let server_pubkey = raw2str(self.server_pubkey.as_bytes());
+        let peer_private_key = raw2str(self.peer_keypair.private.as_bytes());
+        let peer_pubkey = raw2str(self.peer_keypair.public.as_bytes());
+        let peer_ssid = self.peer_ssid.clone();
+        let peer_passwd = self.peer_passwd.clone();
+
+        let value: Value = json!({
+            "peer_ip": peer_ip,
+            "server_socket": server_socket,
+            "server_pubkey": server_pubkey,
+            "peer_private_key": peer_private_key,
+            "peer_pubkey": peer_pubkey,
+            "peer_ssid": peer_ssid,
+            "peer_passwd": peer_passwd
+        });
+        value
+    }
+}
